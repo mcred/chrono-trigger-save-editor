9677f0eb111064642692001cda01ea043d02bf29
Derek Smart <derek@grindaga.com>
2020-07-11 11:06:40 -0400

    checksum close but not passing test
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/storage/GameSave.go
+++ internal/app/storage/GameSave.go
@@ -26,19 +26,46 @@ func (g *Game) GetValue(a Attribute) int {
 	return r
 }
 
+func (g *Game) SetValue(a Attribute, v int) {
+	if a.Is16Bit {
+		b := make([]byte, 2)
+		binary.LittleEndian.PutUint16(b, uint16(v))
+		g.Data[a.Location] = b[0]
+		g.Data[a.Location + 1] = b[1]
+	} else {
+		g.Data[a.Location] = byte(v)
+	}
+}
+
+
 func Open(path string) Game {
 	f, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Printf("Could not open %s", path)
+		fmt.Println("Could not open #{path}")
 		os.Exit(1)
 	}
 	return Game{Data:f}
 }
 
-func Save() {
-	genrateChecksum()
+func (g *Game) Save() {
+	generateChecksum(g)
 }
 
-func genrateChecksum() {
-
+func generateChecksum(g *Game) {
+	for slot := 1; slot <= 3; slot ++ {
+		checksum := 0
+		max := slot * 0xA00
+		min := max - 0xA00
+		for i := min; i < max; i++ {
+			if checksum > 0xFFFF {
+				checksum -= 0xFFFF
+			}
+			if i % 2 == 0 {
+				checksum += g.GetValue(Attribute{i, true})
+			}
+		}
+		l := 0x1FF0 + ((slot - 1) * 2)
+		g.SetValue(Attribute{l, true}, checksum)
+	}
 }
---
+++ tests/app/GameSave_test.go
@@ -0,0 +1,70 @@
+package app__test
+
+import (
+	"ChronoTrigger/internal/app/storage"
+	"reflect"
+	"testing"
+)
+
+var path1 = "../files/save1.sav"
+var path2 = "../files/save2.sav"
+var game1 = storage.Open(path1)
+var game2 = storage.Open(path2)
+
+func TestCanLoad(t *testing.T) {
+	if reflect.TypeOf(game1) != reflect.TypeOf(storage.Game{}) {
+		t.Error("storage.Game Assertion Failed")
+	}
+}
+
+func TestCanGet8BitValue(t *testing.T) {
+	v := game1.GetValue(storage.Attribute{0x203, false})
+	if v != 69 {
+		t.Errorf("GetValue(0x203, false): expected %d, actual %d", 69, v)
+	}
+}
+
+func TestCanGet16BitValue(t *testing.T) {
+	v := game2.GetValue(storage.Attribute{0x203, true})
+	if v != 66 {
+		t.Errorf("GetValue(0x203, false): expected %d, actual %d", 66, v)
+	}
+}
+
+func TestCanSave8BitValue(t *testing.T) {
+	game1.SetValue(storage.Attribute{0x203, false}, 255)
+	v := game1.GetValue(storage.Attribute{0x203, false})
+	if v != 255 {
+		t.Errorf("GetValue(0x203, false): expected %d, actual %d", 255, v)
+	}
+}
+
+func TestCanSave16BitValue(t *testing.T) {
+	game2.SetValue(storage.Attribute{0x203, true}, 2003)
+	v := game2.GetValue(storage.Attribute{0x203, true})
+	if v != 2003 {
+		t.Errorf("GetValue(0x203, true): expected %d, actual %d", 2003, v)
+	}
+}
+
+func TestCanGenerateCheckSum(t *testing.T) {
+	game1.Save()
+	c := game1.GetValue(storage.Attribute{0x1FF0, true})
+	if c != 33610 {
+		t.Errorf("Checksum invalid: expected %d, actual %d", 33610, c)
+	}
+
+	game2.Save()
+	c = game2.GetValue(storage.Attribute{0x1FF0, true})
+	if c != 25819 {
+		t.Errorf("Checksum invalid: expected %d, actual %d", 25819, c)
+	}
+	c = game2.GetValue(storage.Attribute{0x1FF2, true})
+	if c != 25060 {
+		t.Errorf("Checksum invalid: expected %d, actual %d", 25060, c)
+	}
+	c = game2.GetValue(storage.Attribute{0x1FF4, true})
+	if c != 16873 {
+		t.Errorf("Checksum invalid: expected %d, actual %d", 16873, c)
+	}
+}
