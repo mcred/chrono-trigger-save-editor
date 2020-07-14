0e6410c0cf8cbb7d73d0d16b0fef986d819e57d6
Derek Smart <derek@grindaga.com>
2020-07-14 18:44:20 -0400

    can load party inventory
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/party/Party.go
+++ internal/app/party/Party.go
@@ -1,28 +1,40 @@
 package party
 
-import "ChronoTrigger/internal/app/storage"
+import (
+	"ChronoTrigger/internal/app/inventory"
+	"ChronoTrigger/internal/app/storage"
+)
 
-type Party struct{
-	//000 | 256 | Item Type
-	//100 | 256 | Item Count
-	//200 | 560 | Characters (Crono, Marle, Lucca, Robo, Frog, Ayla, Magus, Epoch)
-	Member1 storage.Attribute
-	Member2 storage.Attribute
-	Member3 storage.Attribute
-	SaveCount storage.Attribute
-	Gold storage.Attribute
-	//5B0 | 48  | Names (8 slots, 6 byte C-style strings)
-	MilliSeconds storage.Attribute
-	Seconds storage.Attribute
-	Minutes storage.Attribute
-	Hours storage.Attribute
-	World storage.Attribute
-	PosX storage.Attribute
-	PosY storage.Attribute
+// Item : struct party inventory item structure
+type Item struct {
+	Name  string
+	Count uint
 }
 
+// Party : struct with editable game attributes
+type Party struct {
+	Inventory []Item
+	//000 | 256 | Item Type
+	//100 | 256 | Item Count
+	Member1   storage.Attribute
+	Member2   storage.Attribute
+	Member3   storage.Attribute
+	SaveCount storage.Attribute
+	Gold      storage.Attribute
+	//5B0 | 48  | Names (8 slots, 6 byte C-style strings)
+	MilliSeconds storage.Attribute
+	Seconds      storage.Attribute
+	Minutes      storage.Attribute
+	Hours        storage.Attribute
+	World        storage.Attribute
+	PosX         storage.Attribute
+	PosY         storage.Attribute
+}
+
+// GetParty : Party locations of party attributes in the save file
 func GetParty() Party {
 	return Party{
+		Inventory:    []Item{},
 		Member1:      storage.Attribute{0x580, 1},
 		Member2:      storage.Attribute{0x581, 1},
 		Member3:      storage.Attribute{0x582, 1},
@@ -35,5 +47,18 @@ func GetParty() Party {
 		World:        storage.Attribute{0x5F3, 2},
 		PosX:         storage.Attribute{0x5F5, 1},
 		PosY:         storage.Attribute{0x5F6, 1},
+	}
+}
+
+func (p *Party)LoadInventory(g storage.Game) {
+	for i := 0x0; i <= 0xFF; i++ {
+		v := g.GetValue(storage.Attribute{i, 1})
+		if v != 0{
+			item := Item{
+				inventory.AllItems().GetValByID(v),
+				g.GetValue(storage.Attribute{i + 0x100, 1}),
+			}
+			p.Inventory = append(p.Inventory, item)
+		}
 	}
 }
