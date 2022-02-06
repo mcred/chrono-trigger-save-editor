eb3feac016133f117efd757aa70cf96fde7c3364
Derek Smart <derek@grindaga.com>
2022-02-06 14:28:01 -0500

    init savetools
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ChronoTrigger
 
 go 1.13
+
+require github.com/mcred/savetools v0.0.0-20220206015715-32aaf12df461
--- internal/app/characters/Character.go
+++ internal/app/characters/Character.go
@@ -1,73 +1,92 @@
 package characters
 
-import "ChronoTrigger/internal/app/storage"
+import (
+	"encoding/binary"
+	"github.com/mcred/savetools"
+)
 
 // Character : struct with editable game attributes
 type Character struct {
-	NameID              storage.Attribute
-	CharID              storage.Attribute
-	HP                  storage.Attribute
-	MaxHP               storage.Attribute
-	MP                  storage.Attribute
-	MaxMP               storage.Attribute
-	BasePower           storage.Attribute
-	BaseStamina         storage.Attribute
-	BaseSpeed           storage.Attribute
-	BaseMagic           storage.Attribute
-	BaseHit             storage.Attribute
-	BaseEvade           storage.Attribute
-	BaseMagicDefense    storage.Attribute
-	Level               storage.Attribute
-	Experience          storage.Attribute
-	Helmet              storage.Attribute
-	Armor               storage.Attribute
-	Weapon              storage.Attribute
-	Relic               storage.Attribute
-	ExpToLevel          storage.Attribute
-	CurrentPower        storage.Attribute
-	CurrentStamina      storage.Attribute
-	CurrentSpeed        storage.Attribute
-	CurrentMagic        storage.Attribute
-	CurrentHit          storage.Attribute
-	CurrentEvade        storage.Attribute
-	CurrentMagicDefense storage.Attribute
-	CurrentAttack       storage.Attribute
-	CurrentDefense      storage.Attribute
-	CurrentMaxHP        storage.Attribute
+	NameID              savetools.Attribute
+	CharID              savetools.Attribute
+	HP                  savetools.Attribute
+	MaxHP               savetools.Attribute
+	MP                  savetools.Attribute
+	MaxMP               savetools.Attribute
+	BasePower           savetools.Attribute
+	BaseStamina         savetools.Attribute
+	BaseSpeed           savetools.Attribute
+	BaseMagic           savetools.Attribute
+	BaseHit             savetools.Attribute
+	BaseEvade           savetools.Attribute
+	BaseMagicDefense    savetools.Attribute
+	Level               savetools.Attribute
+	Experience          savetools.Attribute
+	Helmet              savetools.Attribute
+	Armor               savetools.Attribute
+	Weapon              savetools.Attribute
+	Relic               savetools.Attribute
+	ExpToLevel          savetools.Attribute
+	CurrentPower        savetools.Attribute
+	CurrentStamina      savetools.Attribute
+	CurrentSpeed        savetools.Attribute
+	CurrentMagic        savetools.Attribute
+	CurrentHit          savetools.Attribute
+	CurrentEvade        savetools.Attribute
+	CurrentMagicDefense savetools.Attribute
+	CurrentAttack       savetools.Attribute
+	CurrentDefense      savetools.Attribute
+	CurrentMaxHP        savetools.Attribute
+}
+
+func gen8Bit(l int) savetools.Attribute {
+	return savetools.Attribute{
+		Location:   l,
+		Bits:       8,
+		Endianness: binary.LittleEndian,
+	}
+}
+
+func gen16Bit(l int) savetools.Attribute {
+	return savetools.Attribute{
+		Location:   l,
+		Bits:       16,
+		Endianness: binary.LittleEndian,
+	}
 }
 
 // CreateCharacter : Character based off a root position in the save file
 func CreateCharacter(root int) Character {
 	return Character{
-		NameID:              storage.Attribute{root, 1},
-		CharID:              storage.Attribute{root + 0x01, 1},
-		HP:                  storage.Attribute{root + 0x03, 2},
-		MaxHP:               storage.Attribute{root + 0x05, 2},
-		MP:                  storage.Attribute{root + 0x07, 2},
-		MaxMP:               storage.Attribute{root + 0x09, 2},
-		BasePower:           storage.Attribute{root + 0x0B, 1},
-		BaseStamina:         storage.Attribute{root + 0x0C, 1},
-		BaseSpeed:           storage.Attribute{root + 0x0D, 1},
-		BaseMagic:           storage.Attribute{root + 0x0E, 1},
-		BaseHit:             storage.Attribute{root + 0x0F, 1},
-		BaseEvade:           storage.Attribute{root + 0x10, 1},
-		BaseMagicDefense:    storage.Attribute{root + 0x11, 1},
-		Level:               storage.Attribute{root + 0x12, 1},
-		Experience:          storage.Attribute{root + 0x13, 3},
-		Helmet:              storage.Attribute{root + 0x27, 1},
-		Armor:               storage.Attribute{root + 0x28, 1},
-		Weapon:              storage.Attribute{root + 0x29, 1},
-		Relic:               storage.Attribute{root + 0x2A, 1},
-		ExpToLevel:          storage.Attribute{root + 0x2B, 2},
-		CurrentPower:        storage.Attribute{root + 0x36, 1},
-		CurrentStamina:      storage.Attribute{root + 0x37, 1},
-		CurrentSpeed:        storage.Attribute{root + 0x38, 1},
-		CurrentMagic:        storage.Attribute{root + 0x39, 1},
-		CurrentHit:          storage.Attribute{root + 0x3A, 1},
-		CurrentEvade:        storage.Attribute{root + 0x3B, 1},
-		CurrentMagicDefense: storage.Attribute{root + 0x3C, 1},
-		CurrentAttack:       storage.Attribute{root + 0x3D, 1},
-		CurrentDefense:      storage.Attribute{root + 0x3E, 1},
-		CurrentMaxHP:        storage.Attribute{root + 0x3F, 2},
+		NameID:           gen8Bit(root),
+		CharID:           gen8Bit(root + 0x01),
+		HP:               gen16Bit(root + 0x03),
+		MaxHP:            gen16Bit(root + 0x05),
+		MP:               gen16Bit(root + 0x07),
+		MaxMP:            gen16Bit(root + 0x09),
+		BasePower:        gen8Bit(root + 0x0B),
+		BaseStamina:      gen8Bit(root + 0x0C),
+		BaseSpeed:        gen8Bit(root + 0x0D),
+		BaseMagic:        gen8Bit(root + 0x0E),
+		BaseHit:          gen8Bit(root + 0x0F),
+		BaseEvade:        gen8Bit(root + 0x10),
+		BaseMagicDefense: gen8Bit(root + 0x11),
+		Level:            gen8Bit(root + 0x12),
+		//Experience:          gen8Bit(root + 0x13, 3},
+		Helmet:              gen8Bit(root + 0x27),
+		Armor:               gen8Bit(root + 0x28),
+		Weapon:              gen8Bit(root + 0x29),
+		Relic:               gen8Bit(root + 0x2A),
+		ExpToLevel:          gen16Bit(root + 0x2B),
+		CurrentPower:        gen8Bit(root + 0x36),
+		CurrentStamina:      gen8Bit(root + 0x37),
+		CurrentSpeed:        gen8Bit(root + 0x38),
+		CurrentMagic:        gen8Bit(root + 0x39),
+		CurrentHit:          gen8Bit(root + 0x3A),
+		CurrentEvade:        gen8Bit(root + 0x3B),
+		CurrentMagicDefense: gen8Bit(root + 0x3C),
+		CurrentAttack:       gen8Bit(root + 0x3D),
+		CurrentDefense:      gen8Bit(root + 0x3E),
+		CurrentMaxHP:        gen16Bit(root + 0x3F),
 	}
 }
