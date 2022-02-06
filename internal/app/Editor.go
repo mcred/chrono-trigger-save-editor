cb0d40b2b9e60f3494005158b40967ce536438fb
Derek Smart <derek@grindaga.com>
2022-02-06 18:49:32 -0500

    Can edit crono, marle, lucca
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/Editor.go
+++ internal/app/Editor.go
@@ -7,75 +7,12 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 	"github.com/mcred/savetools"
 	"net/url"
 )
-
-//func showCharacterAttributes(c characters.Character, g storage.Game) {
-//	//fmt.Println(utils.DecodeName(c, g))
-//	//fmt.Print("Name ID: ")
-//	//fmt.Println(g.getValue(c.NameID))
-//	//fmt.Print("Character ID: ")
-//	//fmt.Println(g.getValue(c.CharID))
-//	//fmt.Print("Current HP: ")
-//	//fmt.Println(g.getValue(c.HP))
-//	//fmt.Print("Max Hp: ")
-//	//fmt.Println(g.getValue(c.MaxHP))
-//	//fmt.Print("Current MP: ")
-//	//fmt.Println(g.getValue(c.MP))
-//	//fmt.Print("Base Max HP:")
-//	//fmt.Println(g.getValue(c.MaxMP))
-//	//fmt.Print("Base Power: ")
-//	//fmt.Println(g.getValue(c.BasePower))
-//	//fmt.Print("Base Stamina: ")
-//	//fmt.Println(g.getValue(c.BaseStamina))
-//	//fmt.Print("Base Speed: ")
-//	//fmt.Println(g.getValue(c.BaseSpeed))
-//	//fmt.Print("Base Magic: ")
-//	//fmt.Println(g.getValue(c.BaseMagic))
-//	//fmt.Print("Base Hit: ")
-//	//fmt.Println(g.getValue(c.BaseHit))
-//	//fmt.Print("Base Evade: ")
-//	//fmt.Println(g.getValue(c.BaseEvade))
-//	//fmt.Print("Base Magic Defence: ")
-//	//fmt.Println(g.getValue(c.BaseMagicDefense))
-//	//fmt.Print("Level: ")
-//	//fmt.Println(g.getValue(c.Level))
-//	//fmt.Print("Experience: ")
-//	//fmt.Println(g.getValue(c.Experience))
-//	//fmt.Print("Helmet: ")
-//	//fmt.Println(g.getValue(c.Helmet))
-//	//fmt.Print("Armors: ")
-//	//fmt.Println(g.getValue(c.Armor))
-//	//fmt.Print("Weapon: ")
-//	//fmt.Println(g.getValue(c.Weapon))
-//	//fmt.Print("Relic: ")
-//	//fmt.Println(g.getValue(c.Relic))
-//	//fmt.Print("Exp To Level: ")
-//	//fmt.Println(g.getValue(c.ExpToLevel))
-//	//fmt.Print("Current Power: ")
-//	//fmt.Println(g.getValue(c.CurrentPower))
-//	//fmt.Print("Current Stamina: ")
-//	//fmt.Println(g.getValue(c.CurrentStamina))
-//	//fmt.Print("Current Speed: ")
-//	//fmt.Println(g.getValue(c.CurrentSpeed))
-//	//fmt.Print("Current Magic: ")
-//	//fmt.Println(g.getValue(c.CurrentMagic))
-//	//fmt.Print("Current Hit: ")
-//	//fmt.Println(g.getValue(c.CurrentHit))
-//	//fmt.Print("Current Evade: ")
-//	//fmt.Println(g.getValue(c.CurrentEvade))
-//	//fmt.Print("Current Magic Defence: ")
-//	//fmt.Println(g.getValue(c.CurrentMagicDefense))
-//	//fmt.Print("Current Attack: ")
-//	//fmt.Println(g.getValue(c.CurrentAttack))
-//	//fmt.Print("Current Defense: ")
-//	//fmt.Println(g.getValue(c.CurrentDefense))
-//	//fmt.Print("Current Max HP: ")
-//	//fmt.Println(g.getValue(c.CurrentMaxHP))
-//}
 
 //func showPartyAttributes(p party.Party, g storage.Game) {
 //	//fmt.Print("Member1: ")
@@ -211,10 +148,12 @@ func Run() {
 	w := a.NewWindow("Chrono Trigger Save File Editor")
 	ui := &App{app: a, mainWin: w, card: savetools.Card{}, crono: characters.Crono(), marle: characters.Marle(), lucca: characters.Lucca()}
 	ui.init()
-	w.SetContent(container.NewVBox(
-		ui.crono.GenerateForm(&ui.card),
-		ui.marle.GenerateForm(&ui.card),
-		ui.lucca.GenerateForm(&ui.card),
-	))
+	w.SetContent(
+		container.NewVScroll(container.New(layout.NewGridLayout(3),
+			ui.crono.GenerateForm(&ui.card),
+			ui.marle.GenerateForm(&ui.card),
+			ui.lucca.GenerateForm(&ui.card),
+		)),
+	)
 	w.ShowAndRun()
 }
